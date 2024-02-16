e96e655be9621ae7809e0648c66e18ddc6c321a1
mutumagitonga <mutumamwiti11@gmail.com>
2024-02-16 17:30:08 +0300

    Understanding HTML attributes

--- requests_scraper.py
+++ requests_scraper.py
@@ -5,17 +5,21 @@ url = "https://courses.wscubetech.com/"
 faulty_url = "https://courses.wscubetech.comm/"
 unavail_url = "our-site.webflow.io/does-not-exist"
 
+url_test_commerce_site_1 = "https://webscraper.io/test-sites/e-commerce/allinone/computers"
+
 res = None  # Initialize res in global scope (use outside try-except) with a default value
 
 try:
-    print("Fetching data from URL...")
-    res = rq.get(url)  # Getting a webpage
+    print(f"Fetching data from:\n{url_test_commerce_site_1}...")
+    res = rq.get(url_test_commerce_site_1)  # Getting a webpage
     # print(res.status_code)  # Show HTTP Status code
     # print(res.text)  # Show webpage content (HTML of the page)
 except rq.exceptions.RequestException as e:
     print('Stopped:', e)
 except TypeError as e:
     print('Stopped:', e)
+# READ MORE ON EMPLOYING THESE EXCEPTIONS
+
 
 # Parse returned HTML resource using BeautifulSoup library
 soup = None  # Declare & initialize BeautifulSoup object variable in global scope
@@ -26,4 +30,10 @@ if res:
     print("Success! BeautifulSoup object created!")
 else:
     print("BeautifulSoup can't run, response received:", res)
-print(soup)
+
+# Understanding HTML attributes within tags
+header_tag = soup.header  # Access header tag
+header_tag_attrs = header_tag.attrs  # Access header tag attributes
+print(header_tag_attrs['role'])
+
+
