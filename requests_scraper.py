33e663fe9adf00e0932ae1a03fcfee2a189542df
mutumagitonga <mutumamwiti11@gmail.com>
2024-02-16 13:57:20 +0300

    Add information to update user on status

--- requests_scraper.py
+++ requests_scraper.py
@@ -1,15 +1,27 @@
 import requests as rq
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
 url = "https://courses.wscubetech.com/"
 faulty_url = "https://courses.wscubetech.comm/"
+unavail_url = "our-site.webflow.io/does-not-exist"
 
-# Getting a webpage
+res = None  # Initialize res in global scope (use outside try-except) with a default value
+
 try:
-    res = rq.get(url)
-    print(res.status_code)  # Show HTTP Status code
-    print(res.text)  # Show webpage content (HTML of the page)
-except rq.exceptions.ConnectionError as e:
-    print('An error occurred:', e)
+    print("Fetching data from URL...")
+    res = rq.get(url)  # Getting a webpage
+    # print(res.status_code)  # Show HTTP Status code
+    # print(res.text)  # Show webpage content (HTML of the page)
+except rq.exceptions.RequestException as e:
+    print('Stopped:', e)
+except TypeError as e:
+    print('Stopped:', e)
 
 # Parse returned HTML resource using BeautifulSoup library
+if res:
+    print("Data received successfully! Creating a BeautifulSoup object...")
+    # 'lxml' is faster & more lenient HTML parser than bs4 inbuilt 'html.parser'
+    soup = BeautifulSoup(res.text, "lxml")
+    print("Success! BeautifulSoup object created!")
+else:
+    print("BeautifulSoup can't run, response received:", res)
