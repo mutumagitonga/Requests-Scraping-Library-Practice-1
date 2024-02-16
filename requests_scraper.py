897e36c1f8dda66da79a60dd24ca7e25b18bef7f
mutumagitonga <mutumamwiti11@gmail.com>
2024-02-16 12:54:04 +0300

    Install BeautifulSoup library & import it to parse HTML

--- requests_scraper.py
+++ requests_scraper.py
@@ -1,12 +1,15 @@
 import requests as rq
+from bs4 import BeautifulSoup as bs
 
 url = "https://courses.wscubetech.com/"
 faulty_url = "https://courses.wscubetech.comm/"
 
 # Getting a webpage
 try:
-    res = rq.get(faulty_url)
-    print(res.status_code)
+    res = rq.get(url)
+    print(res.status_code)  # Show HTTP Status code
+    print(res.text)  # Show webpage content (HTML of the page)
 except rq.exceptions.ConnectionError as e:
     print('An error occurred:', e)
 
+# Parse returned HTML resource using BeautifulSoup library
