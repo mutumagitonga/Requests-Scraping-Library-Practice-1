a57b74f08399fcd253c37e04ec79b203d497954e
mutumagitonga <mutumamwiti11@gmail.com>
2024-02-16 19:09:02 +0300

    Using BeautifulSoup find() function

--- bs4_find_function.py
+++ bs4_find_function.py
@@ -1,12 +1,12 @@
 import requests as rq
 from bs4 import BeautifulSoup
 
-url = "https://webscraper.iok/test-sites/e-commerce/allinone/computers/tablets"
+url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets"
 
 
 def fetch_html_data(web_address):
     try:
-        print(f"Fetching data {web_address}...")
+        print(f"Fetching data from {web_address}...")
         res = rq.get(web_address)  # Getting a webpage
         return res
     except rq.exceptions.RequestException as e:
@@ -19,7 +19,7 @@ def fetch_html_data(web_address):
 
 def convert_web_data_to_beautiful_soup_obj(web_data):
     try:
-        print("Creating a BeautifulSoup object...")
+        print("Creating BeautifulSoup object...")
         # 'lxml' is faster & more lenient HTML parser than bs4 inbuilt 'html.parser'
         soup_obj = BeautifulSoup(web_data.text, "lxml")
         print("Success! BeautifulSoup object created!")
@@ -32,5 +32,10 @@ response = fetch_html_data(url)  # Fetch web data using requests library
 soup = convert_web_data_to_beautiful_soup_obj(response)  # Web data into BeautifulSoup object
 
 
+def get_price_with_soup_find_function(soup_object):
+    price = soup_object.find("h4", {"class": "float-end price card-title pull-right"})
+    return price.string
 
 
+tablet_price_string = get_price_with_soup_find_function(soup)
+print(tablet_price_string)
