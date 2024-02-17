326d1982b6aa4ffff35c304911600f147527d710
mutumagitonga <mutumamwiti11@gmail.com>
2024-02-17 05:36:58 +0300

    Getting data from all tags specified e.g. ['h4', 'p', 'a']

--- 4_bs4_find_all_with_regex.py
+++ 4_bs4_find_all_with_regex.py
@@ -5,3 +5,22 @@ fetch_url = "https://webscraper.io/test-sites/e-commerce/allinone/computers/tabl
 response = rq_soup.fetch_html_data(fetch_url)
 soup = rq_soup.convert_web_data_to_beautiful_soup_obj(response)
 
+
+def get_data_from_several_tags_once(soup_obj):
+    print("\nGetting data from several tags at once...")
+    mixed_tag_data = soup_obj.find_all(['h4', 'p', 'a'])
+    return mixed_tag_data
+
+
+several_tags_data = get_data_from_several_tags_once(soup)
+print(several_tags_data)
+
+
+def get_all_items_sharing_same_word_in_fullname(soup_obj):
+    print("\nGetting data from items sharing a word in their fullname...")
+    galaxy_tabs = soup_obj.find_all(string=re.compile("Galaxy"))  # Return all tabs with "Galaxy" as part of their names
+    return galaxy_tabs
+
+
+all_galaxy_tabs = get_all_items_sharing_same_word_in_fullname(soup)
+print(all_galaxy_tabs)
